0db124e67223b358c472bc67ca3ec79ba655db74
abcei2 <santiago.salgado.duque@gmail.com>
2023-05-31 09:50:19 -0500

    Intializing repo

---
+++ icons/index.js
@@ -0,0 +1,12 @@
+import { SvgXml } from "react-native-svg";
+
+
+export function SvgComponent() {
+  const svgMarkup = `<svg width="60" height="60" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+  `;
+  const SvgImage = () => <SvgXml xml={svgMarkup} width={"50px"} />;
+
+  return <SvgImage />;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,46 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import NavBar from "../components/NavBar";
+import { SvgComponent } from "../icons";
+import tw from 'twrnc';
 
-export default function App() {
+const Accordion = ({ title, children }) => {
+  const [expanded, setExpanded] = React.useState(false);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+    <View style={tw`border border-gray-400 rounded-md`}>
+      <Pressable
+        style={tw`flex flex-row justify-between items-center px-4 py-2`}
+        onPress={() => setExpanded(!expanded)}
+      >
+        <Text style={tw`text-xl font-semibold`}>{title}</Text>
+        <Text style={tw`text-xl font-semibold`}>{expanded ? "-" : "+"}</Text>
+      </Pressable>
+      {expanded && (
+        <View style={tw`px-4 py-2`}>{children}</View>
+      )}
+    </View>
+  );
+};
+export default function App() {
+  const [amount, setAmount] = React.useState(0);
+  return (
+    <View style={tw`pt-10 bg-green-200 dark:bg-black absolute inset-0`}>
+      <Text style={tw`text-2xl font-extrabold text-center`}>Mesa #1</Text>
+      <View style={tw`flex flex-row items-center gap-1 pl-2`}>
+        <SvgComponent/><Text style={tw`text-xl`}>New table's costumer</Text>
+      </View>
+      <Accordion title="Entradas">
+        <SvgComponent/><Text style={tw`text-xl`}>Add item</Text>
+      </Accordion>
+
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
   text: {
     fontSize: 16,
