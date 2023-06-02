8699d0ef98626d045f0760a09a2e1f3939aa0a96
abcei2 <santiago.salgado.duque@gmail.com>
2023-06-02 08:00:06 -0500

    using talwind with device context

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Table from "../components/Table";
+import tw from 'twrnc';
+import { useDeviceContext } from 'twrnc';
 
 export default function App() {
+  useDeviceContext(tw); 
   return <Table />;
 }
